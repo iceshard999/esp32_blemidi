0101bb014d3264604a25e46c0f60184fc70170af
wks <wks@ustc.edu.cn>
2025-05-21 14:56:04 +0800

    Added a README

--- main/include/blemidi_svc.h
+++ main/include/blemidi_svc.h
@@ -17,6 +17,7 @@
 /* Public function declarations */
 void send_midi_note_on(uint8_t note, uint8_t velocity);
 void send_midi_note_off(uint8_t note);
+
 void gatt_svr_register_cb(struct ble_gatt_register_ctxt *ctxt, void *arg);
 void gatt_svr_subscribe_cb(struct ble_gap_event *event);
 //void gatt_svr_unsubscribe_cb(struct ble_gap_event *event);
